30f7cb817e86afdb8be0a081150cb4ab6740fb98
Piod Gamer <malickelhadji07@gmail.com>
2025-03-27 21:30:47 +0000

    teste

--- config/settings.py
+++ config/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--%d@rw=0e@d+ij&vmft$=9y#x6lp^4hedoi9j0mm0#6u#0+!tg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
 
 
